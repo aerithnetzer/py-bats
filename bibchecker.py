d8f55aa3a3ffc453aa8577f5be91eeaa8ac0d032
aerithnetzer <aerithnetzer@outlook.com>
2023-12-13 12:09:32 -0600

    Bibchecker working

--- bibchecker.py
+++ bibchecker.py
@@ -1,31 +1,34 @@
-import requests
+import logging
+from habanero import Crossref
 import bibtexparser
 
 def check_bib_entries(bib_file):
+    # Configure logging
+    logging.basicConfig(filename='api_log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+
     # Load the .bib file using bibtexparser
     with open(bib_file, 'r') as f:
         bib_database = bibtexparser.load(f)
 
+    # Create a Crossref client
+    cr = Crossref()
+
     # Iterate over all entries in the .bib file
     for entry in bib_database.entries:
         # Get the title and author of the entry
-        title = entry.get('title', '')
-        author = entry.get('author', '')
+        title = entry.get('title', '').replace('{', '').replace('}', '')
+        author = entry.get('author', '').replace('{', '').replace('}', '').replace('(', '').replace(')', '')
 
-        # Call the API to check if the entry is valid
-        response = requests.get(f'https://api.openalex.org/works?search={title}&{author}')
-
-        # Check the response status code
-        if response.status_code == 200:
-            # The API returned a successful response
-            result = response.json()
-            if result['valid']:
-                print(f'{entry["ID"]} is valid')
-            else:
-                print(f'{entry["ID"]} is invalid: {result["error"]}')
+        # Search for the DOI using the title and author
+        results = cr.works(query=title, query_author=author)
+        if 'items' in results['message'] and len(results['message']['items']) > 0:
+            doi = results['message']['items'][0].get('DOI')
+            logging.info(f"Title: {title}, Author: {author}, DOI: {doi}")
+            print("Title: ", title)
+            print("Author: ", author)
+            print("DOI: ", doi)
         else:
-            # The API returned an error
-            print(f'Error checking {entry["ID"]}: {response.status_code}')
+            logging.info(f"No DOI found for title: {title}")
+            print("No DOI found for title: ", title)
 
-# Example usage
-check_bib_entries('/Users/aerith/Library/CloudStorage/OneDrive-NorthwesternUniversity/Documents/warlock/py_bats/production/3/index.bib')
+check_bib_entries('/Users/aerith/warlock/py_bats/production/1/index.bib')
